de6f4c88c726985c633c4984c7a3824a35a93903
Rahul kumar sah <rahulraj12777@gmail.com>
2025-08-07 04:11:47 +0530

    first commit

---
+++ newsapp/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+import React, { Component } from 'react'
+import Navbar from './Componenets/Navbar';
+import News from './Componenets/News';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <News/>
+       
+      </div>
+    )
+  }
+}
+
+
+
