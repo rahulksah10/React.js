bec5018f7008542e57103579555c1e978abf2bc6
Rahul kumar sah <rahulraj12777@gmail.com>
2025-08-28 23:26:39 +0530

    Updtaing

--- Paste-App/src/Redux/PasteSlice.jsx
+++ Paste-App/src/Redux/PasteSlice.jsx
@@ -6,8 +6,8 @@ import toast from 'react-hot-toast';
 const initialState = {
   value: 0,
   pastes: localStorage.getItem("pastes")
-  ? JSON.parse(localStorage.getItem("pastes"))
-  :[]
+    ? JSON.parse(localStorage.getItem("pastes"))
+    : []
 }
 
 export const PasteSlice = createSlice({
@@ -16,19 +16,48 @@ export const PasteSlice = createSlice({
   reducers: {
     addToPastes: (state, action) => {
       const paste = action.payload;
+
+      // add check to find paste is already existing or not
+
       state.pastes.push(paste);
-      localStorage.setItem("pastes", 
+      localStorage.setItem("pastes",
         JSON.stringify(state.pastes))
       toast("Paste created succesfully! ")
-      
+
     },
     updateToPastes: (state, action) => {
-      
+      const paste = action.payload;
+      const index = state.pastes.findIndex((item) =>
+        item._id === paste._id);
+      if (index >= 0) {
+        state.pastes[index] = paste;
+
+        localStorage.setItem("pastes", JSON.stringify(state.pastes));
+
+        toast.success("Paste updated");
+      }
+
     },
     resetAllPastes: (state, action) => {
-      
+
+      state.pastes = [];
+      localStorage.removeItem("Pastes");
+
     },
     removeFromPastes: (state, action) => {
+      const pasteId = action.payload;
+
+      console.log(pasteId);
+      const index = state.pastes.findIndex((item) =>
+        item._id === pasteId);
+
+      if (index >= 0) {
+        state.pastes.splice(index, 1);
+
+        localStorage.pasteItem("pastes", JSON.stringify(state.pastes))
+         
+        toast.success("Paste deleted")
+      }
 
     },
   },
--- Paste-App/src/component/Paste.jsx
+++ Paste-App/src/component/Paste.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeFromPastes } from '../Redux/PasteSlice';
 
 const Paste = () => {
+
+  const pastes = useSelector((state) =>
+    state.paste.pastes);
+
+  const [serchTerm, setsearchterm] = useState('')
+
+  const dispatch = useDispatch();
+
+  const filteredData = pastes.filter((paste) =>
+    paste.title.toLowerCase().includes(serchTerm.toLowerCase())
+  );
+
+
+  function handleDelete(pasteId) {
+    dispatch(removeFromPastes(pasteId));
+  }
+
+
   return (
     <div>
-      <h1>i am paste</h1>
+      <input
+        className='border rounded-2xl min-w-[600px] mt-4 p-2'
+        type="search"
+        placeholder='search here'
+        value={serchTerm}
+        onChange={(e) => setsearchterm(e.target.value)}
+      />
+
+      <div className='flex flex-col gap-3 ml-4'>
+        {
+          filteredData.length > 0 &&
+          filteredData.map(
+            (paste) => {
+              return (
+                <div className='border mt-5 p-2'>
+                  <div >
+                    {paste.title}
+                  </div>
+                  <div>
+                    {paste.content}
+                  </div>
+                  <div className=' flex  flex-row place-content-evenly '>
+                    <button className='border px-3 py-2 rounded-full cursor-pointer '>Edit</button>
+                    <button className='border px-3 rounded-full py-2 cursor-pointer '>View</button>
+                    <button
+                      onClick={() => handleDelete(paste?._id)}
+                      className='border px-3 rounded-full py-2 cursor-pointer '>Delete</button>
+                    <button className='border px-3 rounded-full py-2 cursor-pointer '>Copy</button>
+                    <button className='border px-3 rounded-full py-2 cursor-pointer' >share</button>
+
+                  </div>
+
+                  <div>
+                    {paste.createdAt}
+                  </div>
+                </div>
+
+              )
+            }
+          )
+        }
+      </div>
     </div>
   )
 }
